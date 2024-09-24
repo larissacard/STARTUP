47d4885c6b0425533e43a5767fd6da18311c2e87
larissa-kamily-brisa <larissa.kamily@grupobrisanet.com.br>
2024-09-24 16:20:11 -0300

    refactor: ajustando graficos

--- BASIC-APLICATION/dashboard.py
+++ BASIC-APLICATION/dashboard.py
@@ -1,16 +1,22 @@
-import sqlite3
 import matplotlib.pyplot as plt
+import sqlite3
+import numpy as np
 from datetime import datetime, timedelta
 import pandas as pd
-from matplotlib.gridspec import GridSpec
-import numpy as np
 
 def gerar_faturamento_diario(passeio, dias):
     np.random.seed(len(passeio)) 
-
     faturamento_base = np.random.randint(500, 2000)
     variacao_diaria = np.random.normal(0, 100, dias)
     return np.round(np.cumsum(variacao_diaria) + faturamento_base, 2)
+
+
+def gerar_agendamentos_diarios(passeio, dias):
+    np.random.seed(len(passeio))  
+    agendamentos_base = np.random.randint(10, 50)  
+    variacao_diaria = np.random.normal(0, 5, dias) 
+    return np.round(np.cumsum(variacao_diaria) + agendamentos_base, 0)
+
 
 def obter_dados_relatorios(empresa_id):
     conn = sqlite3.connect('crajubar.db')
@@ -25,7 +31,7 @@ def obter_dados_relatorios(empresa_id):
     conn.close()
     return dados
 
-def obter_dados_7dias(empresa_id):
+def obter_faturamento_7dias(empresa_id):
     dados = obter_dados_relatorios(empresa_id)
     hoje = datetime.now()
 
@@ -38,49 +44,59 @@ def obter_dados_7dias(empresa_id):
 
     return df
 
+def obter_agendamentos_7dias(empresa_id):
+    dados = obter_dados_relatorios(empresa_id)
+    hoje = datetime.now()
+
+    dias = 7
+    datas = [hoje - timedelta(days=i) for i in range(dias)][::-1]
+    df = pd.DataFrame({'data': datas})
+
+    for passeio in [dado[0] for dado in dados]:
+        df[passeio] = gerar_agendamentos_diarios(passeio, dias)
+
+    return df
+
 def renderizar_graficos(empresa_id):
     dados = obter_dados_relatorios(empresa_id)
     nomes_passeios = [dado[0] for dado in dados]
     faturamentos = [dado[1] for dado in dados]
-    agendamentos = [dado[4] for dado in dados]
+    agendamentos = [dado[4] for dado in dados] 
 
-    fig = plt.figure(figsize=(12, 8))
-    gs = GridSpec(2, 2, height_ratios=[1, 1])
+    plt.rcParams["axes.prop_cycle"] = plt.cycler(
+        color=["#4C2A85", "#BE96FF", "#957DAD", "#5E366E", "#A98CCC"])
 
-    ax1 = fig.add_subplot(gs[0, 0])
-    ax1.bar(nomes_passeios, faturamentos)
-    ax1.set_title('Faturamento por Passeio')
-    ax1.set_xlabel('Passeio')
-    ax1.set_ylabel('Faturamento (R$)')
-    plt.setp(ax1.get_xticklabels(), rotation=45, ha="right", fontsize=10)
+    fig, axes = plt.subplots(2, 2, figsize=(15, 10))  
+    fig.suptitle('Dashboard de Relatórios', fontsize=16)
 
-    ax2 = fig.add_subplot(gs[0, 1])
-    barras = ax2.barh(list(nomes_passeios), agendamentos)
-    ax2.set_title('Agendamentos por Passeio')
-    ax2.set_xlabel('Nº Agendamentos')
+    axes[0, 0].bar(nomes_passeios, faturamentos)
+    axes[0, 0].set_title("Faturamento por Passeio")
+    axes[0, 0].set_xlabel("Passeio")
+    axes[0, 0].set_ylabel("Faturamento")
 
-    for barra in barras:
-        ax2.annotate(f'{int(barra.get_width())}', xy=(barra.get_width(), barra.get_y() + barra.get_height() / 2),
-                     xytext=(5, 0), textcoords='offset points', ha='left', va='center')
+    axes[0, 1].barh(nomes_passeios, faturamentos)
+    axes[0, 1].set_title("Inventário por Passeio")
+    axes[0, 1].set_xlabel("Faturamento")
+    axes[0, 1].set_ylabel("Passeio")
 
-    plt.setp(ax2.get_yticklabels(), fontsize=10)
+    df_faturamento = obter_faturamento_7dias(empresa_id)
 
-    df = obter_dados_7dias(empresa_id)
-    ax3 = fig.add_subplot(gs[1, :])
-    for coluna in df.columns[1:]:
-        ax3.plot(df['data'], df[coluna], marker='o', label=coluna)
+    for coluna in df_faturamento.columns[1:]:
+        axes[1, 0].plot(df_faturamento['data'], df_faturamento[coluna], marker='o', label=coluna) 
+    axes[1, 0].set_title('Crescimento/Decrescimento de Faturamento nos Últimos 7 Dias', fontsize=12)
+    axes[1, 0].set_xlabel('Data', fontsize=10)
+    axes[1, 0].set_ylabel('Faturamento (R$)')
+    axes[1, 0].legend(loc='upper left')
 
-    ax3.set_title('Crescimento/Decrescimento de Faturamento nos Últimos 7 Dias')
-    ax3.set_xlabel('Data')
-    ax3.set_ylabel('Faturamento (R$)')
-    ax3.legend(loc='upper left')
-    ax3.grid(True)
+    df_agendamentos = obter_agendamentos_7dias(empresa_id)
 
-    fig.autofmt_xdate()
+    for coluna in df_agendamentos.columns[1:]:  
+        axes[1, 1].plot(df_agendamentos['data'], df_agendamentos[coluna], marker='o', label=coluna)  
+    axes[1, 1].set_title('Crescimento/Decrescimento de Agendamentos nos Últimos 7 Dias', fontsize=12)
+    axes[1, 1].set_xlabel('Data', fontsize=10)
+    axes[1, 1].set_ylabel('Agendamentos')
+    axes[1, 1].legend(loc='upper left')
 
-    plt.subplots_adjust(wspace=0.4, hspace=0.4)
+    plt.tight_layout(rect=[0, 0.03, 1, 0.95])
 
- 
-    plt.tight_layout()
     plt.show()
-
